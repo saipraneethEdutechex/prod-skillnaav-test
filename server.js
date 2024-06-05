96875770f804a990a7dcb37ebed392ab7f04a59a
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-06-05 18:31:05 +0530

    removed node_modules

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(express.json());
 app.use("/api/skillnaav", skillnaavRoute);
 
 const port = process.env.PORT || 5000;
+const path = require("path");
+
+if (process.env.NODE_ENV ===  "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+}
 
 const dbConfig = require("./config/dbConfig");
 
